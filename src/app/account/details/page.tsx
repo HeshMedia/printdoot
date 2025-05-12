8dd0c0b021683b6be2a9e055a1e5368e29fbb225
HarnoorSingh1234 <harnoorsingharora2005@gmail.com>
2025-05-12 20:34:55 +0530

    fixed build error

---
+++ src/app/account/details/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AccountDetailsPage() {
+  return (
+    <div>AccountDetailsPage</div>
+  )
+}
+
+export default AccountDetailsPage
---
+++ src/app/account/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AccounntPage() {
+  return (
+    <div>AccounntPage</div>
+  )
+}
+
+export default AccounntPage
