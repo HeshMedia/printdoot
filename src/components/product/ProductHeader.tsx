3a6524a3bcb4a849f58d59be51aba267285517e5
HarnoorSingh1234 <harnoorsingharora2005@gmail.com>
2025-05-14 16:13:35 +0530

    heading fixed bug

--- src/components/product/ProductHeader.tsx
+++ src/components/product/ProductHeader.tsx
@@ -79,7 +79,7 @@ const ProductHeader: React.FC<ProductHeaderProps> = ({
       {/* Title & Price Section */}
       <div className="flex flex-col md:flex-row md:items-start md:justify-between gap-4">
         <div>
-          <h1 className="text-3xl font-bold text-gray-900 break-words max-w-full md:max-w-2xl">
+          <h1 className="text-3xl font-bold text-gray-900 break-words max-w-full ">
             {productName}
           </h1>
           
